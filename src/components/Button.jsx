a376a806b2c82aabe6f08aeec844b060334a8b3e
Domisnnet <dominique.goncalves61@gmail.com>
2025-01-21 11:48:27 -0300

    simplificação no método onClick

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Button.css';
+import '../components/Button.css';
 
 const Button = (props) => {
   const classes = ['button'];
@@ -11,7 +11,7 @@ const Button = (props) => {
   return (
     <button 
       className={classes.join(' ')}
-      onClick={(e) => props.click && props.click(props.label)}
+      onClick={props.click}
     >
       {props.label}
     </button>
