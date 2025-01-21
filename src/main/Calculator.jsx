66c0d3fb73da6135a22ea0060e2fb2ae7684af83
Domisnnet <dominique.goncalves61@gmail.com>
2025-01-21 10:46:49 -0300

    retirado comentáreo em calculator.jsx

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -6,11 +6,11 @@ import './Calculator.css';
 
 export default class Calculator extends Component {
     addDigit = (n) => {
-      console.log(n);
+      
     }
 
     setOperation = (op) => {
-        console.log(op);
+        
     }
 
     clearMemory = () => {
@@ -19,28 +19,28 @@ export default class Calculator extends Component {
 
   render() {
     const addDigit = n => this.addDigit(n);
-    const setOperation = op => this.setOperation(op); // <== Corrigido 'n' por 'op'
+    const setOperation = op => this.setOperation(op); 
 
     return (
       <div className="calculator">
         <Display value={"100"} />
         <Button Label="AC" click={() => this.clearMemory()} triple />
-        <Button Label="/" click={() => setOperation('/')} operation />  {/*  <== Propriedade Operation Adicionada */}
+        <Button Label="/" click={() => setOperation('/')} operation /> 
         <Button Label="7" click={() => addDigit('7')} />
-        <Button Label="8" click={() => addDigit('8')} /> {/*  <== Propriedade click Adicionada */}
-         <Button Label="9" click={() => addDigit('9')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="*" click={() => setOperation('*')} operation />   {/*  <== Propriedade Operation Adicionada */}
-        <Button Label="4" click={() => addDigit('4')} /> {/*  <== Propriedade click Adicionada */}
-         <Button Label="5" click={() => addDigit('5')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="6" click={() => addDigit('6')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="-" click={() => setOperation('-')} operation />    {/*  <== Propriedade Operation Adicionada */}
-         <Button Label="1" click={() => addDigit('1')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="2" click={() => addDigit('2')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="3" click={() => addDigit('3')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="+" click={() => setOperation('+')} operation /> {/*  <== Propriedade Operation Adicionada */}
-        <Button Label="0" click={() => addDigit('0')} double />  {/*  <== Propriedade click Adicionada */}
-        <Button Label="." click={() => addDigit('.')} /> {/*  <== Propriedade click Adicionada */}
-        <Button Label="=" click={() => setOperation('=')} operation /> {/*  <== Propriedade Operation Adicionada */}
+        <Button Label="8" click={() => addDigit('8')} /> 
+         <Button Label="9" click={() => addDigit('9')} /> 
+        <Button Label="*" click={() => setOperation('*')} operation />  
+        <Button Label="4" click={() => addDigit('4')} /> 
+         <Button Label="5" click={() => addDigit('5')} /> 
+        <Button Label="6" click={() => addDigit('6')} /> 
+        <Button Label="-" click={() => setOperation('-')} operation />    
+         <Button Label="1" click={() => addDigit('1')} /> 
+        <Button Label="2" click={() => addDigit('2')} /> 
+        <Button Label="3" click={() => addDigit('3')} /> 
+        <Button Label="+" click={() => setOperation('+')} operation /> 
+        <Button Label="0" click={() => addDigit('0')} double />  
+        <Button Label="." click={() => addDigit('.')} /> 
+        <Button Label="=" click={() => setOperation('=')} operation /> 
       </div>
     );
   }
