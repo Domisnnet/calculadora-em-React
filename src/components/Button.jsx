eea37dc2a3fe058e511bed4a4fbd6a264ce1d04d
Domisnnet <dominique.goncalves61@gmail.com>
2025-01-23 11:52:14 -0300

    correção do bug , botões não apareciam os dígitos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './Button.css';
 
-const Button = (props) => {
-    const classes = ['button'];
+const Button = props => {
+  const classes = ['button'];
 
-    if (props.operation) classes.push('operation');
-    if (props.double) classes.push('double');
-    if (props.triple) classes.push('triple');
+  if (props.operation) {
+    classes.push('operation');
+  }
 
-    return (
-        <button
-        className={classes.join(' ')}
-        onClick={props.click}
-        >
-        {props.label}
-        </button>
-    );
+  if (props.double) {
+    classes.push('double');
+  }
+
+  if (props.triple) {
+    classes.push('triple');
+  }
+
+  return (
+    <button onClick={props.click} className={classes.join(' ')}>
+        {props.Label} 
+    </button>
+  );
 };
 
 export default Button;
